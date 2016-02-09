1f699ea33b0324c3fe61af064341ebb33970725f
metodi8atanasov <metodi8atanasov@abv.bg>
2016-02-09 20:06:44 +0200

    Модул 02, Видео 01 - Добавяне на коментари

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -7,19 +7,22 @@ namespace Modul02
 		public static void Main (string[] args)
 		{
 			//Дефиниране на променливи
-			int a = 0, b = 0;
+			int a = 0, b = 0, c = 0;
 			int sum = 0;
 
 			//Събиране на числа
-			a = 55;
-			b = 10;
+			a = 10;
+			b = 5;
+			c = 15;
 
-			sum = a + b; 
+			sum = a + b + 'c'; 
 
 			//Писане в конзола
 			Console.Write ( a );
 			Console.Write (" + ");
-			Console.Write (b);
+			Console.Write ( b );
+			Console.Write (" + ");
+			Console.Write ( c );
 			Console.Write (" = ");
 			Console.WriteLine ( sum );
 		}
